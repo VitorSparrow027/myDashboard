44803a30569c28c31c89023504a3d1cbbf5bd6aa
VitorSparrow027 <55920025+VitorSparrow027@users.noreply.github.com>
2019-10-28 21:51:22 -0300

    srdjhg,jgtdc

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,20 @@
-const  LOGIN_EMAIL = 'admin@email.com.br';
-const  LONGIN_SENHA = '123logar';
+const LOGIN_EMAIL = 'admin@email.com.br';
+const LONGIN_SENHA = '123logar';
 
-function efetuarLogin(){
+function efetuarLogin() {
 
+   if ($("#email").val() == LOGIN_EMAIL && $("#password").val() == LONGIN_SENHA) {
+      toastr.success('Bem vindo a nossa Dashboard!!');
 
- if($("#email").val() == LOGIN_EMAIL && $("#password").val() == LONGIN_SENHA){
-    toastr.success('email correto!!');
- }else if($("#email").val() != LOGIN_EMAIL){
-    toastr.error('Por favor preencha corretamente seu email.!');
- }else{
-    toastr.error('Por favor preencha corretamente sua senha.!');
- }
+      setTimeout(function () { location.href = "index.html"}, 5000);
+      
+      
+
+   } else if ($("#email").val() != LOGIN_EMAIL) {
+      toastr.error('Por favor preencha corretamente seu email.!');
+   } else {
+      toastr.error('Por favor preencha corretamente sua senha.!');
+   }
 }
 
 
